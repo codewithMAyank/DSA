c096268715e99b07c2e430b64f30916016500a8a
Gaurav <royg48848@gmail.com>
2022-08-14 21:01:43 +0530

    multiply

--- input2.java
+++ input2.java
@@ -2,10 +2,9 @@ import java.util.*;
 public class input2 {
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            int sum = a+b;
-            System.out.println(sum);
+            float rad = sc.nextFloat();
+            float area = 3.14f * rad * rad;
+            System.out.println(area);
         }
 
     }
