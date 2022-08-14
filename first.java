bc94e2e5509b7dea89db9d1778f64478803f7466
Gaurav <royg48848@gmail.com>
2022-08-14 11:02:04 +0530

    java commit

---
+++ first.java
@@ -0,0 +1,15 @@
+public class first{
+    public static void main(String[] args) {
+        System.out.println("***");//line ka space 
+        System.out.println("**");
+        System.out.println("*");
+        int a=10;
+        int b=5;
+        System.out.println(a);
+        System.out.println(b);
+        String name= "Tony stark";
+        System.out.println(name);
+
+    }
+}
+//boilercode plate 
