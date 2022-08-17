f9fac3779768436fb57ad5b3310f622eed5ab233
Gaurav <royg48848@gmail.com>
2022-08-17 22:29:13 +0530

    if

---
+++ ifelse.java
@@ -0,0 +1,18 @@
+import java.lang.Thread.State;
+import java.util.*;
+
+public class ifelse {
+    public static void main(String[] args) {
+        int age=18;
+        if (age<17) {
+            System.out.print("not attemd");
+        }
+        if (age>=18) {
+            System.out.print("attend");
+        }
+        else {
+            System.out.print("get out");
+        }
+    }
+    
+}
