1cdacc92066125d589da03e6e5f7eb4652886946
Gaurav <royg48848@gmail.com>
2022-08-17 20:05:18 +0530

    fast add

---
+++ fast.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class fast {
+    public static void main(String[]args) {
+        int a = 10;
+        a+=5;
+        System.out.println(a);
+        
+    }
+    
+}
