6c9fab8fcb3ec584fa178d3f86b1a5c104fe5feb
Gaurav <royg48848@gmail.com>
2022-08-18 21:55:06 +0530

    agin2

--- fast.java
+++ fast.java
@@ -3,8 +3,8 @@ import java.util.*;
 public class fast {
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
-            int number = sc.nextInt();
-            if (number%2==0){
+            int num = sc.nextInt();
+            if (num%2==0){
                 System.out.println("it is even bro");
             } else {
                 System.out.print("it's odd bro");
