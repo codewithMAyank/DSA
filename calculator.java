c20689e5768eb842dbcb0cde89f3c6fe6d9f98b2
Gaurav <royg48848@gmail.com>
2022-08-19 19:25:32 +0530

    final java calculator

--- calculator.java
+++ calculator.java
@@ -1,12 +1,12 @@
 import java.util.*;
-public class calculater {
+public class calculator {
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
-            System.out.println("enter a -->");
+            System.out.print("enter a -->");
             int a = sc.nextInt();
-            System.out.println("enter b -->");
+            System.out.print("enter b -->");
             int b = sc.nextInt();
-            System.out.println("what calculation you want -->54");
+            System.out.print("what calculation you want -->");
             char operator = sc.next().charAt(0);
             switch (operator){
                 case '+' : System.out.println(a+b);
