f69af5dd66b43d99162751f00b08756da9e3d529
Gaurav <royg48848@gmail.com>
2022-08-21 08:54:40 +0530

    loop

---
+++ loop.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class loop {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int range = sc.nextInt();
+            int counter = 1;
+            while(counter <= range) {
+                System.out.println(counter+"");
+                counter++; 
+            }
+        }
+    }
+}
