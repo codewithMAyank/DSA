344eb4ad3a7251be1c57455858268ccb16a1417a
Gaurav <royg48848@gmail.com>
2022-08-23 11:36:22 +0530

    modifyied loop

--- pattern.java
+++ pattern.java
@@ -3,5 +3,11 @@ public class pattern {
         for (int line=1; line<=4;line++){
             System.out.println("****");
         }
+        int n = 123456;
+        while (n > 0){
+            int lastdigit = n % 10;
+            System.out.print(lastdigit);
+            n = n / 10;
+        }
     }
 }
