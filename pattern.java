4f221394b9caf77fd1b196fa0b2077e890686983
Gaurav <royg48848@gmail.com>
2022-08-23 11:45:32 +0530

    agin modify

--- pattern.java
+++ pattern.java
@@ -4,8 +4,10 @@ public class pattern {
             System.out.println("****");
         }
         int n = 123456;
+        int rev = 0;
         while (n > 0){
             int lastdigit = n % 10;
+            rev = (rev*10) + lastdigit;
             System.out.print(lastdigit);
             n = n / 10;
         }
