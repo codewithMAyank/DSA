7ef9dcd2be23c17fb297fc29c83fdc25199355fd
Gaurav <royg48848@gmail.com>
2022-08-23 14:30:39 +0530

    again get out command

--- ifelse.java
+++ ifelse.java
@@ -1,16 +1,17 @@
 import java.util.*;
 public class ifelse {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int age = sc.nextInt();
-        if (age<=17) {
-            System.out.println("not attemd");
-        }
-        if (age>=18) {
-            System.out.println("attend");
-        }
-        else {
-            System.out.println("get out");
+        try (Scanner sc = new Scanner(System.in)) {
+            int age = sc.nextInt();
+            if (age<=17) {
+                System.out.println("not attemd");
+            }
+            if (age>=18) {
+                System.out.println("attend");
+            }
+            else {
+                System.out.println("get out");
+            }
         }
     }
     
