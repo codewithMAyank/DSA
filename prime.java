c9271c8fed03ac3aa134521a4e78a2fd5a7a7319
Gaurav <royg48848@gmail.com>
2022-08-25 13:45:19 +0530

    prime or wot

--- prime.java
+++ prime.java
@@ -3,17 +3,22 @@ public class prime {
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
             int n = sc.nextInt();
-            boolean isprime=true;
-            for (int i=2; i<=n; i++){
-                if (n % i ==0){
-                    isprime=false;
+            if (n==2){
+                System.out.println("it's prime");
+            }else{
+                boolean isprime=true;
+                for (int i=2; i<=n-1; i++){
+                    if (n % i ==0){
+                        isprime=false;
+                    }
+                }
+                if (isprime == true){
+                    System.out.println("its prime number");
+                } else {
+                    System.out.println("its not prime");
                 }
             }
-            if (isprime == true){
-                System.out.println("its prime number");
-            } else {
-                System.out.println("its not prime");
-            }
         }
+    
     }
 }
