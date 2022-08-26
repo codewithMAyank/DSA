eda416de48619b6354ebcefd01310b257f2c5d1e
Gaurav <royg48848@gmail.com>
2022-08-26 18:29:41 +0530

    pattern 1

--- pattern1.java
+++ pattern1.java
@@ -1,6 +1,6 @@
 public class pattern1 {
     public static void main(String[] args) {
-        for (int line=1; line<=4; line++){
+        for (int line=1; line<=12; line++){
             for (int star=1; star<=line ; star++){
                 System.out.print("*");
             }
