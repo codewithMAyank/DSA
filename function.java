bd6523692fc827659aaeb295cc6bc664170b4d2d
Gaurav <royg48848@gmail.com>
2022-08-27 20:43:50 +0530

    function part b1 in my way

--- function.java
+++ function.java
@@ -1,13 +1,14 @@
 import java.util.*;
 public class function {
     public static void printhello() {
-        System.out.println("hello world of functions");
+        System.out.println("hello_world_of_functions");
     }
     public static void printelse() {
-        System.out.println("mind your ass");
+        System.out.println("mind_your_own_business");
     }
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("enter_your_age -->");
             int age = sc.nextInt();
             if (age<=17) {
                 printhello();
