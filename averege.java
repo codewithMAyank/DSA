d4978443377b87e798109dd72d91ea30842e3b69
Gaurav <royg48848@gmail.com>
2022-09-11 00:40:13 +0530

    golmal

--- averege.java
+++ averege.java
@@ -1,10 +1,10 @@
 public class averege {
-    public static void average (int a, int b, int c ) {
-        int average = (a + b + c) / 3;
-        System.out.println(average);
+    public static void avg (int a, int b, int c ) {
+        int avg = (a + b + c) / 3;
+        System.out.println(avg);
         
     }
     public static void main(String[] args) {
-        average(2,2,2);
+        avg(2,2,2);
     }
 }
