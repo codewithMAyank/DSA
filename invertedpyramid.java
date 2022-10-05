84ba8de25ce8d27b4df3d74dddb6789a4ad64430
Gaurav <royg48848@gmail.com>
2022-10-05 11:52:09 +0530

    inverted half number pyramid

---
+++ invertedpyramid.java
@@ -0,0 +1,13 @@
+public class invertedpyramid {
+    public static void inverted(int n) {
+        for (int i=1;i<=n;i++){
+            for (int j=1;j<=n-i+1;j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        inverted(8);
+    }
+}
