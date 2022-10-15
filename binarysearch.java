3487ce237fa36490523fcdd0d34f2ac8c7ce77d1
Gaurav <royg48848@gmail.com>
2022-10-15 18:32:42 +0530

    binary search take 1 action

--- binarysearch.java
+++ binarysearch.java
@@ -13,5 +13,26 @@ public class binarysearch {
     // mid==key matlab beeach ka , return output
     // mid>key left me he 1st half , end update
     // mid < key right me he 2nd half , start update
-
+    public static int merabinary(int number[],int key) {
+        int start = 0 ,end = number.length-1;
+        while (start <=end) {
+            int mid = start+end/2;
+            //comparision krne ki bari
+            if (number[mid]==key){
+                System.out.println(mid);
+            }
+            if (number[mid]>key){//left
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] number={1,2,45,99,46,6,55,97};
+        int key = 2;
+        System.out.println("index search in binary"+merabinary(number,key));
+    }
 }
