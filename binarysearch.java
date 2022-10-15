249de8b373ab6a0486cb1a42aa96e3f32a2f1a25
Gaurav <royg48848@gmail.com>
2022-10-15 18:11:13 +0530

    update notes

--- binarysearch.java
+++ binarysearch.java
@@ -5,11 +5,11 @@ public class binarysearch {
     // start index 0
     // elements 7
     // end 7
-    // start 0 end 1 
+    // start 0 end 7 
     // end=n-1
     // while (start<=end)
     // find mid = start + end/2
-    // compare mid witj key
+    // compare mid with key
     // mid==key matlab beeach ka , return output
     // mid>key left me he 1st half , end update
     // mid < key right me he 2nd half , start update
