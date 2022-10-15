e035d1f545c38551c23bdca81397e16c5d779fc4
Gaurav <royg48848@gmail.com>
2022-10-15 14:44:14 +0530

    binary ki shuruat

---
+++ binarysearch.java
@@ -0,0 +1,17 @@
+public class binarysearch {
+    // dictionary wala search
+    // binary search
+    // sorted arrray acending or decending
+    // start index 0
+    // elements 7
+    // end 7
+    // start 0 end 1 
+    // end=n-1
+    // while (start<=end)
+    // find mid = start + end/2
+    // compare mid witj key
+    // mid==key matlab beeach ka , return output
+    // mid>key left me he 1st half , end update
+    // mid < key right me he 2nd half , start update
+
+}
