be217eddf0768161dd8bfaff0e8ffb9bae19be94
Gaurav <royg48848@gmail.com>
2022-10-16 17:20:47 +0530

    pair array take 3 action

--- pairsarray.java
+++ pairsarray.java
@@ -3,13 +3,16 @@ public class pairsarray {
     //jese (1,2) like something
     //repeat nahi hona chahiye
     public static void pairs(int [] numbers) {
+        int totalpair=0;
         for (int i=0;i<numbers.length;i++){
             for (int j=i+1;j<numbers.length;j++){
                 System.out.print("(" + numbers[i] + "," + numbers[j] + ")");
+                totalpair++;
 
             }
             System.out.println();
         }
+        System.out.println("total pairs =>"+totalpair);
     }
     public static void main(String[] args) {
         int[] numbers = {1,4,6,9,2,3};
