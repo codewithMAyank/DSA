0ae52d22c35d1dabc1ce7d77a368353742dca910
Gaurav <royg48848@gmail.com>
2022-10-16 17:17:34 +0530

    pair array take 2

--- pairsarray.java
+++ pairsarray.java
@@ -4,9 +4,8 @@ public class pairsarray {
     //repeat nahi hona chahiye
     public static void pairs(int [] numbers) {
         for (int i=0;i<numbers.length;i++){
-            int add = numbers[i];
             for (int j=i+1;j<numbers.length;j++){
-                System.out.print("(" + add + "," + numbers[j] + ")");
+                System.out.print("(" + numbers[i] + "," + numbers[j] + ")");
 
             }
             System.out.println();
