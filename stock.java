c4b24036f52458847ba314135b909b1df6b24a2b
Gaurav <royg48848@gmail.com>
2022-10-22 11:54:12 +0530

    stock take 1 action

---
+++ stock.java
@@ -0,0 +1,10 @@
+public class stock {
+    public static void stock_up_and_down(int [] stock_price) {
+        // Integer.MAX_VALUE = + infinity
+        // Integer.MIN_VALUE = - infinity
+        
+    }
+    public static void main(String[] args) {
+        int [] stock_price= {40,6,78,93,100,37};
+    }
+}
