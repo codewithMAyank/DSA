b2a6e0f5622b3e144df876daa7da983b8861d4ba
Gaurav <royg48848@gmail.com>
2022-10-24 09:39:36 +0530

    stock action 3

--- stock.java
+++ stock.java
@@ -6,11 +6,16 @@ public class stock {
         int max_profit= 0;
         for (int i= 0;i<stock_price.length;i++){
             if (buy_price<stock_price[i]){
-                int profit = stock_price
+                int profit = stock_price[i] - buy_price;
+                max_profit = Math.max(max_profit,buy_price);
+            } else {
+                buy_price= stock_price[i];
             }
         }
+        return max_profit;
     }
     public static void main(String[] args) {
         int [] stock_price= {40,6,78,93,100,37};
+        System.out.println(stock_up_and_down(stock_price));
     }
 }
