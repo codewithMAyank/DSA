983f7207743f000510f96e9351f3b84b155c113f
Gaurav <royg48848@gmail.com>
2022-10-29 14:04:44 +0530

    2d array take 1 action

---
+++ array_two.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class array_two {
+    public static void main(String[] args) {
+        int matrix [][] = new int [3][3];
+        int n = 3, m = 3;
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0 ;i<n;i++){
+            for (int j=0;j<m;j++){
+                matrix[i][j]= sc.nextInt();
+            }
+        }
+    }
+}
