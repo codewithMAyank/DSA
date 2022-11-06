aecc0db8f9c2afddaeeca4c3b1f64bc3af4502da
Gaurav <royg48848@gmail.com>
2022-11-06 15:57:28 +0530

    1st string creation

--- strings.java
+++ strings.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class strings {
     public static void main(String[] args) {
-        
+        // int arr[]= {1,2,3,4,5,6};
+        // char brr[]={'a','s','d','f','g','h'};
+        // String  crr[]= {"qwerty"};
+        //string is immutable 
+        Scanner sc = new Scanner(System.in);
+        //scanner ka function
+        String Name;
+        Name = sc.nextLine();
+        System.out.println(Name);
+
+
     }
 }
