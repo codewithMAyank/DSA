063bf4cf9949a110d84e74e2fce25f6c86206e60
Gaurav <royg48848@gmail.com>
2023-01-07 18:34:09 +0530

    New Year Start

---
+++ old.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.Arrays;
+import java.util.Scanner;
+import javax.xml.transform.Source;
+
+public class old {
+    public static void main(String[] args) {
+        System.out.println("hello world again");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("input number :->");
+        int age = sc.nextInt();
+        System.out.println(age);
+    }
+}
