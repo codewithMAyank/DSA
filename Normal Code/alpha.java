77b88301ff6532a7196bab0fb1beee28d4ea886e
Gaurav <royg48848@gmail.com>
2023-01-15 06:49:32 +0530

    modified

--- Normal Code/alpha.java
+++ Normal Code/alpha.java
@@ -1,6 +1,6 @@
 public class alpha {
     public static void main(String[] args) {
-        int n = 4;
+        int n = 6;
         char ch = 'A';
         for (int i=1;i<=n;i++){
             for (int j=1;j<=i;j++){
