5defa1a602588e0b46e34057671f85ded1e29aeb
Gaurav <royg48848@gmail.com>
2023-02-09 10:17:34 +0530

    first attempt

--- UOP/firstop.java
+++ UOP/firstop.java
@@ -3,3 +3,38 @@
 // 2. constructors 
 // 3.destructor
 // 4.inheritors 
+// starting of classes and objects
+/**
+ * firstop
+ */
+public class firstop {
+
+    
+}
+/**
+ * Pen
+ */
+class Pen {
+
+    //prperties + functions
+    String color;
+    int tip;
+    void setcolor (String newcolor ){
+        color= newcolor;
+    }    
+    void settip (int newtip){
+        tip= newtip;
+    }
+}
+/**
+ * student
+ */
+class student {
+    String name;
+    int age;
+    float percentage;//cgpa
+    void clcmarks (int phy,int chem,int math){
+        percentage= ( phy+ chem + math)/3;
+    }
+    
+}
