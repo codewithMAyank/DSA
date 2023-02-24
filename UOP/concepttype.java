aaddf5e4e05253b6dcb85c1482556dc803acf76d
Gaurav <royg48848@gmail.com>
2023-02-24 07:48:15 +0530

    constructor types in java

--- UOP/concepttype.java
+++ UOP/concepttype.java
@@ -8,15 +8,17 @@ public class concepttype {
 //--------------------------------------------------------------------------------------------------------------
     // constructor invoked at time of object cration
     // like pen p1= new Pen();
-    //pen name ka constructor
-    //only function pen name ka 
-    //memory allocation after contructor called
+    // pen name ka constructor
+    // only function pen name ka 
+    // memory allocation after contructor called
     /**
     * studentss
      */
     public static void main(String[] args) {
         studentss s1= new studentss("Gaurav");
+        studentss s2= new studentss(1234);
         System.out.println(s1.name);
+        System.out.println(s2.Roll);
     }
 }
 class studentss {
@@ -27,5 +29,12 @@ class studentss {
         this.name= name;
         // System.out.println();
     }
-
+    studentss(int Roll){
+        this.Roll=Roll;
+    }
+    //constructor overloading = konsi object ke liye konsa constructor called
+    // types of constructor
+    // 1. non 
+    // 2. parameterized
+    // 3. Copy constructor
 }
