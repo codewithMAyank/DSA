2ce4b001c517ec4bdd2d5e5f193839e239469dfb
Gaurav <royg48848@gmail.com>
2023-02-28 10:23:46 +0530

    just kidding

--- UOP/packeges.java
+++ UOP/packeges.java
@@ -3,8 +3,8 @@ import java.util.*;
 
 public class packeges {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        //class object keyword contructor parameter
+        try (Scanner sc = new Scanner(System.in)) {
+        }
         
 
     }
