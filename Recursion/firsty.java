9f5efe0f181efa6baf552dbf3de639828064a5ea
Gaurav <royg48848@gmail.com>
2023-03-02 12:35:52 +0530

    firstly recurssion

--- Recursion/firsty.java
+++ Recursion/firsty.java
@@ -2,6 +2,7 @@ public class firsty {
 // static given value
 //decresing order
     public static void printDec(int n) {
+        //base case
         if (n == 1){
             System.out.println(n);
             return;
@@ -9,7 +10,9 @@ public class firsty {
         System.out.print(n + " ");
         printDec(n-1);
     }
+    //increase number
     public static void printINC(int n) {
+        //base case
         if (n == 1){
             System.out.print(n+" ");
             return;
@@ -17,11 +20,25 @@ public class firsty {
         printINC(n-1);
         System.out.print(n+" ");
     }
+
+    // factorial n
+    public static int fact(int n) {
+        //base case
+        if (n == 0){
+            return 1;
+        }
+        int fnm1 = fact(n-1);
+        int fn = n* fact(n-1);
+        return fn;
+        
+    }
+
 // main order
     
     public static void main(String[] args) {
         int n = 10;
         printDec(n);
         printINC(n);
+        System.out.println(fact(n));
     }
 }
