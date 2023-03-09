439cdf4880ca080bfdf06407a87fea6483741122
Gaurav <royg48848@gmail.com>
2023-03-09 15:33:42 +0530

    recurssion consecutive one binary

---
+++ Recursion/recbinary.java
@@ -0,0 +1,21 @@
+public class recbinary {
+    
+    public static void generateStrings(int n, String s) {
+        if (n == 0) {
+            System.out.println(s);
+            return;
+        }
+        
+        if (s.length() > 0 && s.charAt(s.length()-1) == '1') {
+            generateStrings(n-1, s + "0");
+        } else {
+            generateStrings(n-1, s + "0");
+            generateStrings(n-1, s + "1");
+        }
+    }
+    
+    public static void main(String[] args) {
+        int n = 4;
+        generateStrings(n, "");
+    }
+}
