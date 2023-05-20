15e5378698c9e5abd604108989c0952e8831aacb
Gaurav <royg48848@gmail.com>
2023-05-20 21:05:49 +0530

    updated succesful first backtracing array

--- backtracking/backtracking.java
+++ backtracking/backtracking.java
@@ -1,27 +1,29 @@
 package backtracking;
 
 public class backtracking {
-    public static void changearr(int arr[], int i, int val) {
-        //base case
-        if (i==arr.length) {
+    public static void changeArr(int[] arr, int i, int val) {
+        // base case
+        if (i == arr.length) {
             printArr(arr);
             return;
         }
-        //recurssion
-        arr[i] = val;
-        changearr(arr, i+1, val+1);
-        arr[i]=arr[i]-2;
         
+        // recursion
+        arr[i] = val;
+        changeArr(arr, i + 1, val + 1);
+        arr[i] -= 2;
     }
-    public static void printArr(int arr[]) {
-        for (int i = 0; i<arr.length;i++){
-            System.out.print(arr[i]+" ");
-            }
-        System.out.println();
+    
+    public static void printArr(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
+        System.out.println();
+    }
+    
     public static void main(String[] args) {
-        int arr[]= new int [5];
-        changearr(null, 0, 1);
+        int[] arr = new int[5];
+        changeArr(arr, 0, 1);
         printArr(arr);
     }
 }
